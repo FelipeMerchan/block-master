4e547359f34bc4dccf04849bc110950124d34ad7
Felipe Merchan <felipe-sc@hotmail.es>
2021-04-24 23:21:16 -0500

    Se agregaron las etiquetas meta para el Open Graph

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,11 @@ class MyDocument extends Document {
           <meta name="robots" content="index,follow" />
           <meta name="description" content="" />
           <meta name="keywords" content="Películas, online, estrenos, streaming" />
+          <meta property="og:title" content="Mi proyecto del curso de flexbox layout y componentes" />
+          <meta property="og:type" content="website" />
+          <meta property="og:description" content="Interfaz de Spotifu, una de las apps más famosas de la industria musical." />
+          <meta property="og:url" content="https://redesign-spotifu.vercel.app/" />
+          <meta property="og:image" content="https://res.cloudinary.com/dy1xcx7kw/image/upload/v1619323822/spotifu/open-graph-spotifu_nqztzl.jpg" />
         </Head>
         <body>
           <Main />
