17098b91b6259bfddbb87567c117b597e0cfa695
Felipe Merchan <felipe-sc@hotmail.es>
2021-05-17 15:03:13 -0500

    Se agregó el wrapper al Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,17 +6,21 @@ import Search from './Search'
 const Header = () => {
   return (
     <header className="header" aria-label="Barra superior y menú del usuario">
-      <figure className="header-logo">
-        <img
-          src="https://res.cloudinary.com/dy1xcx7kw/image/upload/v1621231618/block-master/logo-block-master_2x_iwg5rq.png"
-          width="106"
-          height="64"
-          alt="Logo de Block Master"
-          title="Logo de Block Master"
-        />
-      </figure>
-      <Navigation />
-      <Search />
+      <div className="wrapper">
+        <div className="header-content">
+          <figure className="header-logo">
+            <img
+              src="https://res.cloudinary.com/dy1xcx7kw/image/upload/v1621231618/block-master/logo-block-master_2x_iwg5rq.png"
+              width="106"
+              height="64"
+              alt="Logo de Block Master"
+              title="Logo de Block Master"
+            />
+          </figure>
+          <Navigation />
+          <Search />
+        </div>
+      </div>
     </header>
   )
 }
